49faaf36c24b379b2399ea66709e89705a74d851
Jagadesh-Ronanki <jagadesh.ronanki@gmail.com>
2022-11-03 19:04:48 +0530

    Sophomore-2

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,4 +1,4 @@
 const WHITELIST_CONTRACT_ADDRESS = "0xbdf21726B84033DF04E995ED9c7299BeBe678414";
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://jrnet-nft-collection.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
